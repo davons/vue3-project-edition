84d2411f3bec1b2d8b7e5d509178aa7755cd02d7
Yvon Rakotonanahary <yvon.rakotonanahary@gmail.com>
2025-12-28 20:04:50 +0100

    tanstack table

--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { h, ref } from 'vue'
 import type { Tables } from '../../../database/types'
 import { supabase } from '@/lib/supabaseClient'
+import type { ColumnDef } from '@tanstack/vue-table'
+import DataTable from '@/components/ui/data-table/DataTable.vue'
 
 defineOptions({ name: 'TasksPage' })
 
@@ -22,17 +24,54 @@ const getTasks = async () => {
 ;(async () => {
   await getTasks()
 })()
+
+const columns: ColumnDef<Tables<'task'>>[] = [
+  {
+    accessorKey: 'name',
+    header: () => h('div', { class: 'text-left' }, 'Name'),
+    cell: ({ row }) => {
+      const name = row.getValue('name') as string
+      return h('div', { class: 'text-left font-medium' }, name)
+    },
+  },
+  {
+    accessorKey: 'status',
+    header: () => h('div', { class: 'text-left' }, 'Status'),
+    cell: ({ row }) => {
+      const status = row.getValue('status') as string
+      return h('div', { class: 'text-left font-medium' }, status)
+    },
+  },
+  {
+    accessorKey: 'due_date',
+    header: () => h('div', { class: 'text-left' }, 'Due date'),
+    cell: ({ row }) => {
+      const due_date = row.getValue('due_date') as string
+      return h('div', { class: 'text-left font-medium' }, due_date)
+    },
+  },
+  {
+    accessorKey: 'project_id',
+    header: () => h('div', { class: 'text-left' }, 'Project ID'),
+    cell: ({ row }) => {
+      const project_id = row.getValue('project_id') as string
+      return h('div', { class: 'text-left font-medium' }, project_id)
+    },
+  },
+  {
+    accessorKey: 'collaborators',
+    header: () => h('div', { class: 'text-left' }, 'Collaborators'),
+    cell: ({ row }) => {
+      const collaborators = row.getValue('collaborators') as string
+      return h('div', { class: 'text-left font-medium' }, collaborators)
+    },
+  },
+]
 </script>
 
 <template>
   <div>
-    <h2>Tasks View</h2>
-    <ul v-if="tasks && tasks.length">
-      <li v-for="task in tasks" :key="task.id">
-        <h3>{{ task.name }}</h3>
-      </li>
-    </ul>
-    <RouterLink to="/">Go to Home</RouterLink>
+    <DataTable v-if="tasks" :columns="columns" :data="tasks" />
   </div>
 </template>
 
