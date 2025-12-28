5d60c501513888ecb9a2e6d2daf994e664929b00
Yvon Rakotonanahary <yvon.rakotonanahary@gmail.com>
2025-12-28 12:13:17 +0100

    uplugin route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue'),
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: () => import('../views/ProjectsView.vue'),
-    },
-    {
-      path: '/projects/:id',
-      name: '/projects_details',
-      component: () => import('../views/SingleProjectsView.vue'),
-    },
-
-    {
-      path: '/:catchAll(.*)*',
-      name: 'not-found',
-      component: () => import('../views/NotFoundView.vue'),
-    },
-  ],
+  routes,
 })
 
 export default router
