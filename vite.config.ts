c68cf86d894568cd2c804fb806679224e10f6053
Yvon Rakotonanahary <yvon.rakotonanahary@gmail.com>
2025-12-28 18:58:25 +0100

    nav bar

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import VueRouter from 'unplugin-vue-router/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     VueRouter({
       dts: './typed-router.d.ts',
     }),
