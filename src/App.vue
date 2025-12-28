b00195d5488418213cb1db409eab5c35a6d3eea3
Yvon Rakotonanahary <yvon.rakotonanahary@gmail.com>
2025-12-28 11:09:15 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const counter = ref(0)
+
+const increment = () => {
+  counter.value++
+}
+const decrement = () => {
+  counter.value--
+}
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    <button @click="increment">+</button>
+    <span>{{ counter }}</span>
+    <button @click="decrement">-</button>
+  </p>
+</template>
+
+<style scoped></style>
