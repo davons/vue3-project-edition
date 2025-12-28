0714b957f08a85b1ba04dd4fe1fb1eb65dc5f146
Yvon Rakotonanahary <yvon.rakotonanahary@gmail.com>
2025-12-28 19:44:47 +0100

    main layout

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <script setup lang="ts">
-import Sidebar from './components/layout/Sidebar.vue'
-import TopNavbar from './components/layout/TopNavbar.vue'
-
+import Auth from './components/layout/main/Auth.vue'
 defineOptions({ name: 'App' })
 </script>
 
 <template>
-  <Sidebar />
-  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
-    <TopNavbar />
-    <main>
-      <RouterView />
-    </main>
-  </div>
+  <Auth>
+    <RouterView />
+  </Auth>
 </template>
 
 <style scoped></style>
---
+++ src/components/layout/SidebarLinks.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+interface link {
+  to: string
+  icon: string
+  label: string
+}
+
+defineProps<{
+  links: link[]
+}>()
+</script>
+
+<template>
+  <RouterLink
+    active-class=""
+    exact-active-class="text-primary bg-muted"
+    v-for="link in links"
+    :to="link.to"
+    :key="link.to"
+    class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground"
+  >
+    <iconify-icon :icon="link.icon"></iconify-icon>
+    <span class="hidden lg:block text-nowrap">{{ link.label }}</span>
+  </RouterLink>
+</template>
+
+<style scoped></style>
